f762233719406bbce0f0c6583ec87abd1b8b310f
Irina <postnikova_irina00@inbox.ru>
2020-05-03 14:15:08 +0300

    Profile finished

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-	<header class="mb-6">
+	<header class="mb-6 relative">
 		<img src="../images/default-profile-banner.jpg"
 			alt="banner"
-			class="mb-2" 
+			class="rounded-lg mb-2" 
 		>
 
-		<div class="flex justify-between items-center">
+		<div class="flex justify-between items-center mb-4">
 			<div>
 				<h2 class="font-bold text-2xl">{{ $user->name }}</h2>
 				<p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
@@ -28,14 +28,17 @@
 					Follow Me
 				</a>
 			</div>
-
-			<img 
-				src="{{ $user->avatar }}" 
-				alt="your avatar" 
-				class="rounded-full h-32 w-32 object-cover object-top"
-			>
-			
 		</div>
+
+		<p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat delectus quae, quam assumenda tempora repellat maiores rem deserunt voluptatem, commodi fuga impedit sed fugiat veritatis quasi cumque quas temporibus aspernatur.
+		</p>
+
+		<img 
+			src="{{ $user->avatar }}" 
+			alt="your avatar" 
+			class="rounded-full h-32 w-32 object-cover object-top absolute"
+			style="top: 9.5rem; left: calc(50% - 4rem);" 
+		>
 	</header>
 
 	@include ('_timeline', [
